b59094dc0be4a43c6663c236739072a9b57a6691
najla23 <najla.hosseini@gmail.com>
2025-03-22 17:48:06 +0100

    updated files

--- SAPTDatabase/readXML_ACT_EnergyC_XYZ.py
+++ SAPTDatabase/readXML_ACT_EnergyC_XYZ.py
@@ -3,7 +3,7 @@ import xml.etree.ElementTree as ET
 import os
 import csv
 
-xmlfile = "sapt-f2.xml"
+xmlfile = "sapt-0.015.xml"
 
 dimers = [
     "water#water", "water#bromide", "water#lithium", "water#potassium", "potassium#chloride",
