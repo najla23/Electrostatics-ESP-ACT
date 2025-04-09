55de31bc943a6442ab90478149c7d3bb5cf7ca81
David van der Spoel <david.vanderspoel@gmail.com>
2025-04-09 16:13:24 +0200

    Updated tables and script

--- RMSD_Compound/fix_tab.py
+++ RMSD_Compound/fix_tab.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import os
+import os, sys
 
 table = { "a1dexp": "Induction correction A",
           "bdexp": "Induction correction b",
@@ -13,15 +13,19 @@ table = { "a1dexp": "Induction correction A",
           "zeta": "Gaussian distribution width $\\zeta$",
           "vs3sa": "Virtual site position $a$" }
 
-outt = "output_table_PC+GVS.tex"
-with open(outt, "w") as outf:
-    intt = "output_table_SC-Water-Ion_PC+GVS.tex"
-    with open(intt, "r") as inf:
+if len(sys.argv) != 3:
+    sys.exit("Usage: %s input.tex output.tex" % sys.argv[0])
+    
+if os.path.exists(sys.argv[2]):
+    sys.exit("Will not write over existing file %s please remove manually" % sys.argv[2])
+    
+with open(sys.argv[2], "w") as outf:
+    with open(sys.argv[1], "r") as inf:
         for line in inf:
             lll = line.strip()
             words = lll.split("&")
             if len(words) == 3 and words[0].strip() in table.keys():
-                outf.write("%s & {\\verb %s} & %s\n" % ( table[words[0].strip()],
+                outf.write("%s & \\verb^%s^ & %s\n" % ( table[words[0].strip()],
                                                         words[1].strip(), words[2] ))
             else:
                 outf.write(line)
